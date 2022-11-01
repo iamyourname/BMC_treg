f6d76d637bc8fae95f2c8837d0da087d5eef0994
Moiseev, Mikhail <m.moiseev@x5.ru>
2022-11-01 12:12:46 +0300

    Initial commit

---
+++ src/main/java/com/example/bmc_treg/BmcTregApplication.java
@@ -0,0 +1,13 @@
+package com.example.bmc_treg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmcTregApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmcTregApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bmc_treg/BmcTregApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bmc_treg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmcTregApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
